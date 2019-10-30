6e99c1d34294d9580865bad4d05cd989d5c3e4e3
Jesus_0414 <jesus140400@gmail.com>
2019-10-30 10:06:44 -0700

    shaders

--- helloWorld.js
+++ helloWorld.js
@@ -1,14 +1,52 @@
 const canvas = document.getElementById("glcanvas");
 
+const vsSource = `
+    attribute vec4 aVertexPosition;
+
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+  `;
+
+const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
+
+const loadShader = (gl, type, source)=>{
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+const initShader = (gl, vsSource, fsSource )=>{
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+}
+
 const main = ()=>{
-    const gl = canvas.getContext("webgl");
-    if (!gl)/*(gl === null)*/ {
-        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-        return;
-      }
-      gl.clearColor(0,0,0,1);//RGBA(A=Alpha)
-      gl.clear(gl.COLOR_BUFFER_BIT);
+  const gl = canvas.getContext("webgl");
+
+  if (!gl)/*(gl === null)*/ {
+      alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+      return;
+  }
+
+  initShader(gl,vsSource, fsSource);
+  gl.clearColor(0,0,0,1);//RGBA(A=Alpha)
+  gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
 window.onload = main;
-
